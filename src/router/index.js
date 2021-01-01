2da6be3cd6235745b59bdcfab97ef8a0b1d01143
崔文刚 <1528177831@qq.com>
2021-01-01 21:10:10 +0800

    创建组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import movieRouter from './movie'
+import cinemaRouter from './cinema'
+import mineRouter from './mine'
 Vue.use(VueRouter)
 
-const routes = [
-  
-]
-
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
-  routes
+  routes: [
+    movieRouter,
+    cinemaRouter,
+    mineRouter,
+    {
+      path: '/*',
+      redirect: '/movie'
+    }
+  ]
 })
 
 export default router
